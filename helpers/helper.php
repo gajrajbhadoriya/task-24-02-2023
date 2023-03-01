9f0def1f99a741b2637d1893fb00dd5b1c8e3f7f
gajrajbhadoriya <125948781+gajrajbhadoriya@users.noreply.github.com>
2023-03-01 18:01:10 +0530

    Update helper.php

--- helpers/helper.php
+++ helpers/helper.php
@@ -5,7 +5,7 @@ function getMinMaxVal(array $array, string $col, string $type= 'max')
     $val = $array[array_key_first($array)];
 
     foreach ($array as $arr) {
-        $valIsMinMax = ($type === 'max' && (abs($val[$col]) < abs($arr[$col]))) || ($type === 'min' && (abs($val[$col]) < abs($arr[$col])));
+        $valIsMinMax = ($type === 'max' && (abs($val[$col]) < abs($arr[$col]))) || ($type === 'min' && (abs($val[$col]) > abs($arr[$col])));
         if ($valIsMinMax){
             $val = $arr;
             // continue;
